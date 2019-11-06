1e0cedcf7cf7a421494d3bcf7b5101d98ab8966b
Brad Alexander <brad.alexander913@gmail.com>
2019-11-06 02:17:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
-attr_accessor :name, :breed
-attr_reader :id
+  attr_accessor :name, :breed
+  attr_reader :id
 
-def initialiize(name:,breed:)
-  @name = name
-  @breed = breed
-end
+  def initialize(name:,breed:)
+    @name = name
+    @breed = breed
+  end
 
 end
